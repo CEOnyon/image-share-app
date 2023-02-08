49a87851411736f08bf450b5de6fb909dce53591
Laith Albusairi <laithalbusairi@gmail.com>
2023-02-07 19:33:49 -0800

    added to 404 page

--- frontend/src/components/error.jsx
+++ frontend/src/components/error.jsx
@@ -3,7 +3,10 @@ import React from "react";
 
 const error404 = () =>{
     return(
-        <div></div>
+        <div>
+            <h1>404Page</h1>
+            <p>Redirecting to <span>Sign Up</span></p>
+        </div>
     )
 }
 
