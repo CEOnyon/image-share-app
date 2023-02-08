05df21f9ac29d6f505e0eea7a2d68aec12165cc9
AHenry2117 <aehenry2117@gmail.com>
2023-02-07 22:16:47 -0800

    changed navbar and styled navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Oddballs
-      </header>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,21 +1,31 @@
-import Nav from 'react-bootstrap/Nav';
+import React from 'react'
+import {Container,Nav,Navbar,Button,Form} from 'react-bootstrap'
 
-function bottomNav() {
+function TopNav() {
   return (
-    <>
-      <Nav className="justify-content-center" activeKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/login">Login</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="/gallery">Gallery</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="/upload">Upload</Nav.Link>
-        </Nav.Item>
-      </Nav>
-      </>
+    <Navbar className='navbar' variant='dark' expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Name</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#upload">Upload</Nav.Link>
+          </Nav>
+          <Form className="d-flex" pullRight>
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="search"
+              aria-label="Search"
+            />
+            <Button variant="dark">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default bottomNav;
+export default TopNav;
