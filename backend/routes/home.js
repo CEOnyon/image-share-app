2d9caf825082fbaffce683a3dfe960595d3fdaf5
Winston Grant <winstonf.grant@hotmail.com>
2023-02-08 19:28:45 -0800

    stuff

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -1,5 +1,5 @@
 const router = require(`express`).Router();
 // requiring moongoose model
-const User = require(`../models/users.modelsjs`)
+const User = require(`../models/users.models.js`)
 
 router.route(`/`).get
