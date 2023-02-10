11f9424d72f9b9dc103b354eff4ecdb2e1307b0d
Laith Albusairi <laithalbusairi@gmail.com>
2023-02-09 19:51:43 -0800

    finsihed sign up shell

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -11,11 +11,22 @@ const Signup = () => {
 
     return (
         <form className="signup" onSubmit={handlesubmit}>
-            
+            <h3>Sign up</h3>
 
+            <label>Email:</label>
+            <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+            <label>Password</label>
+            <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
 
-
-
+            <button>Sign up</button>
         </form>
     )
 
