24ad41cad78abf4f40a7f160086af5877cc76eb9
AHenry2117 <aehenry2117@gmail.com>
2023-02-09 22:32:58 -0800

    pulled from master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-// import Signup from './components/Signup';
-// import Login from './components/login';
-// import Error from './components/error.jsx';
-
-function App() {
-  return (
-    <div className="App">
-  
-    {/* <Route
-      path="/"
-      element={<Login/>}
-    />
-
-    <Route
-      path="/"
-      element={<Signup/>}
-    /> */}
-
-
-      {/* <Route path='*'>
-        <Error/>
-        </Route> */}
-    </div>
-    
-=======
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navbar from './components/navbar.jsx';
 
@@ -34,7 +8,6 @@ function App() {
     <>
       <Navbar/>
     </>
->>>>>>> 9ac9538bd8557d794acd83ccbb6c312a111115a1
   );
 }
 
