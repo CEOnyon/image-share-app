0efaacfad6f49e6141cab4ff32152f30b7286cde
Laith Albusairi <laithalbusairi@gmail.com>
2023-02-09 19:52:49 -0800

    pasted to login page

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -31,3 +31,5 @@ const Signup = () => {
     )
 
 }
+
+export default Signup
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import { useState } from 'react'
 
-export default function Login() {
-  return(
-    <form>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  )
+
+const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <form className="signup" onSubmit={handlesubmit}>
+            <h3>Sign up</h3>
+
+            <label>Email:</label>
+            <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+            <label>Password</label>
+            <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
+
+            <button>Sign up</button>
+        </form>
+    )
+
 }
+
+export default Signup
