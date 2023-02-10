2144f811d71fec981adea6c7eaace549910c1eaa
Courtney Hatcher <courtneyehatcher@gmail.com>
2023-02-09 20:21:21 -0800

    more file changes

--- backend/server.js
+++ backend/server.js
@@ -12,30 +12,30 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
-app.get('/api/images', async (req, res) => {
-    const { resources } = await cloudinary.search
-        .expression('folder:dev_setups')
-        .sort_by('public_id', 'desc')
-        .max_results(30)
-        .execute();
+// app.get('/api/images', async (req, res) => {
+//     const { resources } = await cloudinary.search
+//         .expression('folder:dev_setups')
+//         .sort_by('public_id', 'desc')
+//         .max_results(30)
+//         .execute();
 
-    const publicIds = resources.map((file) => file.public_id);
-    res.send(publicIds);
-});
+//     const publicIds = resources.map((file) => file.public_id);
+//     res.send(publicIds);
+// });
 
-app.post('/api/upload', async (req, res) => {
-    try {
-        const fileStr = req.body.data;
-        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-            upload_preset: 'dev_setups',
-        });
-        console.log(uploadResponse);
-        res.json({ msg: 'yaya' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ err: 'Something went wrong' });
-    }
-});
+// app.post('/api/upload', async (req, res) => {
+//     try {
+//         const fileStr = req.body.data;
+//         const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+//             upload_preset: 'dev_setups',
+//         });
+//         console.log(uploadResponse);
+//         res.json({ msg: 'yaya' });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ err: 'Something went wrong' });
+//     }
+// });
 
 
 const port = process.env.PORT
