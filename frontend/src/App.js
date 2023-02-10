53d8380b393dceeac956db354a5a676ad28a4ff8
Laith Albusairi <laithalbusairi@gmail.com>
2023-02-10 00:09:49 -0800

    stopping poin t

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-// import Signup from './components/Signup';
-// import Login from './components/login';
-// import Error from './components/error.jsx';
-
-function App() {
-  return (
-    <div className="App">
-  
-    {/* <Route
-      path="/"
-      element={<Login/>}
-    />
-
-    <Route
-      path="/"
-      element={<Signup/>}
-    /> */}
-
-
-      {/* <Route path='*'>
-        <Error/>
-        </Route> */}
-    </div>
-    
-=======
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navbar from './components/navbar.jsx';
+
 
 function App() {
   return (
     <>
       <Navbar/>
+    
     </>
->>>>>>> 9ac9538bd8557d794acd83ccbb6c312a111115a1
+    
   );
 }
 
