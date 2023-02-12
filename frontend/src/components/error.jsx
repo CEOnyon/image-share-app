c1b10790976937176732cb3aea812c765a75aec4
AHenry2117 <aehenry2117@gmail.com>
2023-02-11 21:43:02 -0800

    merged

--- frontend/src/components/error.jsx
+++ frontend/src/components/error.jsx
@@ -3,8 +3,7 @@ function Error404() {
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p>Oops, sorry, we can't find this page!</p>
-            <img src="./assets/error404.jpg" alt="melted ice cream cone" />
-            Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/photos/52jRtc2S_VE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            <img src="../assets/error404.jpg" alt="melting ice cream cone" />
         </main>
     );
 }
