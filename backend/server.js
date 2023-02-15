c6ad41a6bcb11c83c665962f90287d553a9fa969
Courtney Hatcher <courtneyehatcher@gmail.com>
2023-02-14 18:53:35 -0800

    file changes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
+// Load Local Environment Configuration
+import * as dotenv from "dotenv";
+dotenv.config();
+
 //allows use of express cors and moongoose
 import express from "express";
 import cors from "cors";
 import multer from "multer";
 import {readChunk} from 'read-chunk';
 import imageType, {minimumBytes} from 'image-type';
+import { nanoid } from "nanoid";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
 
-// Setup Express & Uploads
-const app = express();
-const upload = multer({ dest: './uploads/' })
+// Setup Express
+const app = express();              // Setup Express
+app.use(express.json());            // Accept JSON data on post
+app.use((err, req, res, next) => {  // Default Error Handler (Send errors to Console, not to Client)
+  console.error(err.stack)
+  return res.status(500).send('Application Error, Check Logs.');
+});
+
+// Setup Multer
+const upload = multer({  
+    limits: {              // Limits to uploaded content.
+      fileSize: 5242880,   // 5 megabytes in bytes
+    }
+  });
 
 // Setup Middleware
 app.use(express.static('public'));
